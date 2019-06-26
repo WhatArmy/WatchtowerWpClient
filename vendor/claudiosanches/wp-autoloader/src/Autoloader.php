29dc58ee39e6fa04fee212b6b322c1ca8e0a0d09
Code2Prog <office@code2prog.com>
2019-06-26 16:53:15 +0700

    Feature: php 5 compatibility

--- vendor/claudiosanches/wp-autoloader/src/Autoloader.php
+++ vendor/claudiosanches/wp-autoloader/src/Autoloader.php
@@ -37,7 +37,7 @@ class Autoloader
      * @param string $baseDir A base directory for class files in the
      * namespace.
      */
-    public function addNamespace(string $prefix, string $baseDir)
+    public function addNamespace($prefix, $baseDir)
     {
         $prefix  = trim($prefix, '\\') . '\\';
         $baseDir = rtrim($baseDir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
@@ -56,7 +56,7 @@ class Autoloader
      * @return string        The mapped file name on success, or emtpy string on
      * failure.
      */
-    public function loadClass($class): string
+    public function loadClass($class)
     {
         $prefix = $class;
 
@@ -111,7 +111,7 @@ class Autoloader
      * @param  string $path Relative class path.
      * @return string
      */
-    protected function normalizePath(string $path): string
+    protected function normalizePath($path)
     {
         return str_replace('_', '-', strtolower($path));
     }
@@ -122,7 +122,7 @@ class Autoloader
      * @param  string $relativeClass Relative class.
      * @return string
      */
-    protected function getRelativeFile(string $relativeClass): string
+    protected function getRelativeFile($relativeClass)
     {
         // Class file names should be based on the class name with
         // "class-" prepended and the underscores in the class name
@@ -144,7 +144,7 @@ class Autoloader
      * @param  string $file The file to require.
      * @return bool         True if the file exists, false if not.
      */
-    protected function requireFile(string $file): bool
+    protected function requireFile($file)
     {
         if (file_exists($file)) {
             require $file;
