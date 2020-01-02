eb80ab4646e228395064be0d2dbe460d28edd6ee
Code2Prod <office@code2prog.com>
2020-01-02 22:47:52 +0800

    skin compatibility update fix

--- src/class-updater-skin.php
+++ src/class-updater-skin.php
@@ -4,7 +4,7 @@
 namespace WhatArmy\Watchtower;
 
 if (!function_exists('\WP_Upgrader_Skin')) {
-    require_once ABSPATH.'wp-admin/includes/class-wp-upgrader-skin.php';
+    require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader-skin.php';
 }
 
 class Updater_Skin extends \WP_Upgrader_Skin
@@ -27,7 +27,7 @@ class Updater_Skin extends \WP_Upgrader_Skin
     {
     }
 
-    public function feedback($feedback)
+    public function feedback($feedback, ...$args)
     {
         $this->feedback = $feedback;
     }
