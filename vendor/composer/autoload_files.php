17f95e9cf8d2057960bf21f6be6caac93cd9763f
Code2Prod <office@code2prog.com>
2020-04-07 23:57:15 +0700

    fix old php mime

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -9,5 +9,6 @@ return array(
     '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
     '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
     'ef6802c8a38664a4b1e8712ed25377fb' => $vendorDir . '/shuber/curl/curl.php',
+    'def43f6c87e4f8dfd0c9e1b1bab14fe8' => $vendorDir . '/symfony/polyfill-iconv/bootstrap.php',
     '241d2b5b9c1e680c0770b006b0271156' => $vendorDir . '/yahnis-elsts/plugin-update-checker/load-v4p9.php',
 );
